2aa7aebf061cb7463ba67469e9ef01896229e33b
Zyrst <zajreg@gmail.com>
2022-12-01 21:19:32 +0100

    Day 1 + repo

---
+++ AoC2022/Day1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AoC2022
+{
+    internal class Day1
+    {
+
+        public void Run()
+        {
+            var file = File.ReadAllLines("input/input_d1.txt");
+            var elfs = new List<int>();
+            elfs.Add(0);
+            foreach(var line in file)
+            {
+                if(line == "")
+                {
+                    elfs.Add(0);
+                }
+                else
+                {
+                    elfs[elfs.Count - 1] += int.Parse(line);
+                }
+            }
+
+            var max_elf = elfs.Max();
+            elfs.Sort();
+            elfs.Reverse();
+            var t = elfs[0] + elfs[1] + elfs[2];
+            Console.WriteLine($"Max elf: {max_elf}");
+            Console.WriteLine($"Total three: {t}");
+        }
+    }
+}
---
+++ AoC2022/Program.cs
@@ -0,0 +1,5 @@
+﻿using AoC2022;
+
+
+var d1 = new Day1();
+d1.Run();
