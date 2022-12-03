48b35489c7a01f13d5bb3edc1a5d9d4855f32ba4
Zyrst <zajreg@gmail.com>
2022-12-03 10:51:56 +0100

    Day 3 donezo

---
+++ AoC2022/Day3.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC2022
+{
+    internal class Day3
+    {
+        public void Run()
+        {
+            var lines = File.ReadAllLines("input/input_d3.txt");
+            var sum = 0;
+            foreach(var line in lines)
+            {
+                var first = line.Substring(0, line.Length / 2);
+                var second = line.Substring(line.Length / 2);
+                var common = first.Intersect(second).ToArray();
+                var val = CharToVal(common[0]);
+                sum += val;
+            }
+
+            Console.WriteLine(sum);
+
+            sum = 0;
+            // sum = lines.Chunk(3).Select(items => items[0].Intersect(items[1]).Intersect(items[2]).Single()).Sum(CharToVal);  // Nice version on reddit, chuck splits in to the 3 pieces I wanted
+            for (int i = 0; i < lines.Length; i += 3)
+            {
+                var items = lines.Skip(i).Take(3).ToArray();
+                var common = items[0].Intersect(items[1]).Intersect(items[2]).ToArray();
+
+                var val = CharToVal(common[0]);
+                sum += val;
+            }
+            Console.WriteLine(sum);
+        }
+
+        private int CharToVal(char c)
+        {
+            var t = c % 32;
+            var val = Char.IsUpper(c) ? (t + 26) : t;
+            return val;
+        }
+    }
+}
--- AoC2022/Program.cs
+++ AoC2022/Program.cs
@@ -4,5 +4,8 @@
 //var d1 = new Day1();
 //d1.Run();
 
-var d2 = new Day2();
-d2.Run();
+//var d2 = new Day2();
+//d2.Run();
+
+var d3 = new Day3();
+d3.Run();
